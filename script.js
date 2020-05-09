9715fee72903a2bca1e216971b39f05c04515120
Rlong0013 <63034155+Rlong0013@users.noreply.github.com>
2020-05-09 17:45:54 -0400

    add files

---
+++ script.js
@@ -0,0 +1,30 @@
+const video = document.getElementById('video');
+const pipButton = document.getElementById('pipButton');
+if ('pictureInPictureEnabled' in document) {
+  pipButton.classList.remove('hidden')
+  pipButton.disabled = false;
+  
+  pipButton.addEventListener('click', () => {
+    if (document.pictureInPictureElement) {
+      document
+        .exitPictureInPicture()
+        .catch(error => {
+          // Error handling
+        })
+    } else {
+      video
+      .requestPictureInPicture()
+      .catch(error => {
+        // Error handling
+      });
+    }
+  });
+}
+
+video.addEventListener('enterpictureinpicture', () => {
+    pipButton.textContent = 'Exit Picture-in-Picture mode';
+});
+
+video.addEventListener('leavepictureinpicture', () => {
+    pipButton.textContent = 'Enter Picture-in-Picture mode';
+});
